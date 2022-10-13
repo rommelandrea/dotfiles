10b8fe7ee50bfe8a91cb20133db525b4075ee51f
Andrea <rommelandrea@gmail.com>
2022-10-13 13:34:44 +0000

    git add defaut branch

--- script/git.sh
+++ script/git.sh
@@ -3,3 +3,4 @@
 git config --global user.name "Andrea Romanello aka rommel"
 git config --global user.email "rommelandrea@gmail.com"
 git config --global core.editor vim
+git config --global init.defaultBranch main
